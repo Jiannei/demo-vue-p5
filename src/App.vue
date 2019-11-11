3eb66749b21ee929ad3575d5c8e0bb301e0668f1
Jiannei <longjian.huang@aliyun.com>
2019-11-11 23:08:40 +0800

    pref: external file

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,13 @@
 </template>
 
 <script>
+const sketch = require('./sketchs/simple')
 export default {
   name: 'app',
     mounted() {
-      const  sketch = function (p5) {
-          // NOTE: Set up is here
-          p5.setup = function () {
-              let canvas = p5.createCanvas(600, 400);
-              canvas.parent('#canvas');
-          }
-
-          // NOTE: Draw is here
-          p5.draw = function () {
-              p5.background(102);
-              p5.ellipse(50, 50, 80, 80);
-          }
-      }
-      
         // NOTE: Use p5 as an instance mode
         const P5 = require('p5');
-        new P5(sketch)
+        new P5(sketch.main)
     }
 }
 </script>
---
+++ src/sketchs/simple.js
@@ -0,0 +1,13 @@
+export function main(p5) {
+    // NOTE: Set up is here
+    p5.setup = function () {
+        let canvas = p5.createCanvas(600, 400);
+        canvas.parent('#canvas');// Display p5 canvas on optional position
+    }
+
+    // NOTE: Draw is here
+    p5.draw = function () {
+        p5.background(102);
+        p5.ellipse(50, 50, 80, 80);
+    }
+}
