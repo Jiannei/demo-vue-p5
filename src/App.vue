6a662b0151d1731b41b6f3cf6ac063903d65dad4
Jiannei <longjian.huang@aliyun.com>
2019-11-11 23:29:03 +0800

    feat: send a callback into p5.js

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
       <div id="canvas"></div>
+      {{ message }}
   </div>
 </template>
 
 <script>
-import sketch from './sketchs/simple'
+let sketch = require('./sketchs/simple')
 
 export default {
   name: 'app',
+    // NOTE: Add data
+    data() {
+        return {
+            message: ""
+        }
+    },
     mounted() {
         // NOTE: Use p5 as an instance mode
         const P5 = require('p5');
-        new P5(sketch)
-    }
+        new P5(sketch.main)
+
+        // NOTE: p5.js
+        sketch.setDelegate(this.callbackOnP5);
+    },
+    methods: {
+        // NOTE: Add callback method
+        callbackOnP5: function(timeStr) {
+            this.message = timeStr;
+        }
+    },
 }
 </script>
 
--- src/sketchs/simple.js
+++ src/sketchs/simple.js
@@ -1,4 +1,16 @@
-export default function(p5) {
+let delegate;
+let p5;
+
+function notifyCurrentTime() {
+    if (delegate !== undefined) {
+        const message = p5.hour() + ":" + p5.minute() + ":" + p5.second();
+
+        delegate(message);
+    }
+}
+
+export function main(_p5) {
+    p5 = _p5
     // NOTE: Set up is here
     p5.setup = function () {
         let canvas = p5.createCanvas(600, 400);
@@ -9,5 +21,11 @@ export default function(p5) {
     p5.draw = function () {
         p5.background(102);
         p5.ellipse(50, 50, 80, 80);
+
+        notifyCurrentTime();
     }
+}
+
+export function setDelegate(_delegate) {
+     delegate = _delegate;
 }
